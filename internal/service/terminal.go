0a98559333409495cbb3ae73dab5348bba5f82bb
mBigFish <3433148732@qq.com>
2026-08-21 23:31:59 +0800

    初始化

--- internal/service/terminal.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// Terminal 终端会话接口，屏蔽不同操作系统的 PTY 实现差异
-type Terminal interface {
-	Read(buf []byte) (int, error)
-	Write(data []byte) (int, error)
-	Resize(cols, rows uint16) error
-	Close() error
-}
